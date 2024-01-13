86d7c27c1e97fb6ee88e0080ee58ab477de0834f
Fiqre-Ab <Fiqreyesusabraham@gmail.com>
2024-01-12 21:54:57 -0800

    css styling the page

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,30 @@ import data from "./stackline_frontend_assessment_data_2021.json";
 import logo from "./stackline_logo.svg";
 
 function App() {
-
   useEffect(() => {
     console.log(data);
   }, []);
 
   return (
-    <>
-      <img src={logo} className="logo" alt="stackline-Logo" />
-      {
-        data.map((product) => (
-          <div key={product.id}>
-            <h2>{product.title}</h2>
-            <p>{product.subtitle}</p>
-            {/* Render other details as needed */}
+    <div className="app-container">
+      <div className="logoContainer">
+        <img src={logo} className="logo" alt="Stackline Logo" />
+      </div>
+      <div className="main-content">
+        <div className="sidebar">
+          {/**/ }
+          <div key={data[0].id}>
+            <img src={data[0].image} className="product-image" alt={data[0].title} />
+            <h2 className="product-title">{data[0].title}</h2>
+            <p className="product-subtitle">{data[0].subtitle}</p>
+            {/*  */}
           </div>
-        ))
-      }
-    </>
+        </div>
+        <div className="chart-container">
+          {/* The chart related component should be rendered here */}
+        </div>
+      </div>
+    </div>
   );
 }
 
