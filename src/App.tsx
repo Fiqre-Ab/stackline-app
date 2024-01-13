3ab5e4c660562f9e877b061ba0526de092fdcd96
Fiqre-Ab <Fiqreyesusabraham@gmail.com>
2024-01-12 19:40:21 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import data from "./stackline_frontend_assessment_data_2021.json";
+import logo from "./stackline_logo.svg";
+
+function App() {
+  useEffect(() => {
+    console.log(data);
+  }, []);
+
+  return (
+    <>
+      <img src={logo} className="logo" alt="stackline-Logo" />
+      {
+        data.map((product) => (
+          <div key={product.id}>
+            <h2>{product.title}</h2>
+            <p>{product.subtitle}</p>
+            {/* Render other details as needed */}
+          </div>
+        ))
+      }
+    </>
+  );
+}
+
+export default App;
